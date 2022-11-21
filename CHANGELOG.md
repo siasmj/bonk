8f17fd5373393bc1a992eed4cfb63a309e5c6f73
Matt Sias <mattsias@gmail.com>
2022-11-20 20:11:35 -0600

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.0.1] - 2022-11-20
+
+- Gem created
---
+++ lib/bonk.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bonk/version"
+
+module Bonk
+  class Error < StandardError; end
+  # Your code goes here...
+end
